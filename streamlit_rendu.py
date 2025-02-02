cdaf8c2ecf448214441eac606ccde169c597b544
projetgroupehappyda2024 <projetgroupehappyda2024@gmail.com>
2025-02-02 19:41:56 +0100

    Update streamlit_rendu.py

--- streamlit_rendu.py
+++ streamlit_rendu.py
@@ -26,7 +26,7 @@ def load_data():
         return data, data_avant
     except FileNotFoundError:
         st.error("Le fichier 'happycommun1.csv' est introuvable")
-        return None
+        return pd.DataFrame(), pd.DataFrame()
 
 data, data_avant = load_data()
 
